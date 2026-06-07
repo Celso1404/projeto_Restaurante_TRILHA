912f8796476bb83cf7bfdfde9623f254f7103e4e
Celso Bitello <celsokaiiki@gmail.com>
2026-06-07 19:23:24 -0300

    MY26 - Criando um plugin para ler e exibir um arquivo de imagem

--- app.js
+++ app.js
@@ -9,35 +9,52 @@ var redis = require('redis');
 var redisClient = redis.createClient();
 var formidable = require('formidable');
 var path = require('path');
-
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 
 var app = express();
 app.use(function(req, res, next){
-  if(req.method === 'POST'){
-    var form = formidable.Incomingform({
-      uploadDir:path.join(__dirname, "/public/images"),
-      keepExtensions:true
+  let contentType = req.headers["content-type"];
+
+  if(req.method === 'POST' && contentType && contentType.startsWith('multipart/form-data')){
+    var form = new formidable.IncomingForm({
+      uploadDir: path.join(__dirname, "/public/images"),
+      keepExtensions: true
     });
     form.parse(req, function(err, fields, files){
-      req.fields = fields;
-      req.files = files;
+      let bodyData = {};
+      for (let key in fields) {
+        bodyData[key] = Array.isArray(fields[key]) ? fields[key][0] : fields[key];
+      }
 
+      let filesData = {};
+      for (let key in files) {
+        let file = Array.isArray(files[key]) ? files[key][0] : files[key];
+        
+        filesData[key] = {
+          ...file,
+          path: file.filepath || file.path,
+          name: file.originalFilename || file.name
+        };
+      }
+      req.body = bodyData;
+      req.fields = bodyData;
+      req.files = filesData;
       next();
-    })
+    });
+
   } else {
     next();
   }
-})
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(session({
-  store: new RedisStore({
-    client: redisClient
-  }),
+  //store: new RedisStore({
+    //client: redisClient
+  //}),
   secret:'p@ssw@ord',
   resave:true,
   saveUninitialized:true 
--- inc/menus.js
+++ inc/menus.js
@@ -1,5 +1,5 @@
 let conn = require('./db');
-let path = require ('path')
+let path = require('path');
 
 module.exports = {
     getMenus() {
@@ -11,29 +11,73 @@ module.exports = {
                     reject(err);
                   }
                  resolve(results);
-                })
-        })
+                });
+        });
     },
 
     save(fields, files) {
         return new Promise((resolve, reject)=> {
-            fields.photo = `images/${path.parse(files.photo.path).base}`;
+            if (files && files.photo && files.photo.size > 0) {
+                fields.photo = `images/${path.parse(files.photo.path).base}`;
+            } else {
+                delete fields.photo;
+            }
 
-            conn.query(`
-                INSERT INTO tb_menus (title, description, price, photo)
-                VALUES(?, ?, ?, ?)
-            `, [
-                fields.title,
-                fields.description,
-                fields.price,
-                fields.photo
-            ], (err, results)=> {
+            let query, queryParams;
+
+            if (parseInt(fields.id) > 0) {
+                // É UMA EDIÇÃO!
+                
+                if (fields.photo) {
+                    query = `
+                        UPDATE tb_menus 
+                        SET title = ?, description = ?, price = ?, photo = ?
+                        WHERE id = ?
+                    `;
+                    queryParams = [
+                        fields.title,
+                        fields.description,
+                        fields.price,
+                        fields.photo,
+                        fields.id
+                    ];
+                } else {
+                    query = `
+                        UPDATE tb_menus 
+                        SET title = ?, description = ?, price = ?
+                        WHERE id = ?
+                    `;
+                    queryParams = [
+                        fields.title,
+                        fields.description,
+                        fields.price,
+                        fields.id
+                    ];
+                }
+
+            } else {
+                if (!fields.photo) {
+                     fields.photo = 'images/boxed-bg.jpg'; 
+                }
+
+                query = `
+                    INSERT INTO tb_menus (title, description, price, photo)
+                    VALUES(?, ?, ?, ?)
+                `;
+                queryParams = [
+                    fields.title,
+                    fields.description,
+                    fields.price,
+                    fields.photo
+                ];
+            }
+            conn.query(query, queryParams, (err, results)=> {
                 if(err) {
                     reject(err);
                 } else {
-                    resolve(results)
+                    resolve(results);
                 }
             });
-        })
+        });
     }
-}
+};
