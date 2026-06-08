2f158529e5ff3d0fe0925e1c56093181d86dfbe4
Celso Bitello <celsokaiiki@gmail.com>
2026-06-08 01:40:38 -0300

    MY40 - Administração dos usuários criando telas

--- inc/users.js
+++ inc/users.js
@@ -39,7 +39,7 @@ module.exports = {
     getUsers() {
         return new Promise((resolve, reject) => {
             conn.query(`
-                SELECT * FROM tb_users ORDER BY title
+                SELECT * FROM tb_users ORDER BY name
             `, (err, results)=> {
                 if (err) {
                     reject(err);
@@ -52,30 +52,28 @@ module.exports = {
     save(fields, files) {
         return new Promise((resolve, reject)=> {
             
-            let query, queryPhoto = '', params = [
+            let query, params = [
                 fields.name,
-                fields.email,
+                fields.email
             ];
-
             if(parseInt(fields.id) > 0) {
                 params.push(fields.id);
 
                 query = `
                     UPDATE tb_users
                     SET name = ?,
-                        email = ?,
+                        email = ?
                     WHERE id = ?    
                 `;
             } else {
                 query = `
-                    INSERT INTO tb_users (name, emai, password)
+                    INSERT INTO tb_users (name, email, password)
                     VALUES(?, ?, ?)
                 `;
 
-                params.push(fields.password)
+                params.push(fields.password);
             }
-
-            conn.query(query, queryParams, (err, results)=> {
+            conn.query(query, params, (err, results)=> { 
                 if(err) {
                     reject(err);
                 } else {
