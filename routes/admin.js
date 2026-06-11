617706e45a16c1bd63c2c6f209721a593cabf276
Celso Bitello <celsokaiiki@gmail.com>
2026-06-10 23:56:12 -0300

    MY_EXTRA - Refatorando as rotas para alocar a função async

--- routes/admin.js
+++ routes/admin.js
@@ -1,213 +1,243 @@
 var express = require("express");
 var users = require("./../inc/users");
-var admin = require("./../inc/admin")
+var admin = require("./../inc/admin");
 var menus = require("./../inc/menus");
 var reservations = require("../inc/reservations");
 const contacts = require("../inc/contacts");
-var emails = require ("./../inc/emails")
+var emails = require("./../inc/emails");
 var moment = require("moment");
 var router = express.Router();
 
 module.exports = function(io){
     moment.locale("pt-BR");
 
-router.use(function(req, res, next){
-    if (['/login'].indexOf(req.url) === -1 && !req.session.user) {
-        res.redirect("/admin/login");
-    } else {
+    router.use(function(req, res, next){
+        if (['/login'].indexOf(req.url) === -1 && !req.session.user) {
+            res.redirect("/admin/login");
+        } else {
+            next();
+        }
+    });
+
+    router.use(function(req, res, next) {
+        req.menus = admin.getMenus(req);
         next();
-    }
-})
-
-router.use(function(req, res, next) {
-    req.menus = admin.getMenus(req);
-    next();
-})
-
-router.get("/", function(req, res, next) {
-    admin.dashboard().then(data => {
-        res.render("admin/index", admin.getParams(req, {
-            data: data 
-        }));
-    }).catch(err => {
-        
-    })
-});
-
-router.get("/dashboard", function(req, res, next){
-    reservations.dashboard().then(data=> {
-        res.json(data); 
-    }).catch(err => {
-        res.status(500).json(err);
     });
-});
 
-router.get("/logout", function(req, res, next) {
-    delete req.session.user;
-    res.redirect("/admin/login")
-}) 
-
-router.post("/login", function(req, res, nxt) {
-    if(!req.body.email) {
-        users.render(req, res, "Preencha o campo e-mail");
-    } else if (!req.body.password) {
-        users.render(req, res, "Preencha o campo senha.");
-    } else {
-        users.login(req.body.email, req.body.password).then(user => {
-            req.session.user = user;
-            res.redirect("/admin");
-        }).catch(err => {
-            users.render(req, res, err.message || err);
-        })
-    }
-})
-
-router.get("/login", function(req, res, next) {
-    users.render(req, res, null);
-})
-
-router.get("/contacts", function(req, res, next) {
-    contacts.getContacts().then(data=>{
-        res.render("admin/contacts", admin.getParams(req, {
-            data
-        }));
+    router.get("/", async function(req, res, next) {
+        try {
+            let data = await admin.dashboard();
+            res.render("admin/index", admin.getParams(req, {
+                data: data 
+            }));
+        } catch (err) {
+            next(err);
+        }
     });
-});
 
-router.delete("/contacts/:id", function(req, res, next){
-    contacts.delete(req.params.id).then(results=>{
-        io.emit('dashboard update')
-        res.send(results);
-    }).catch(err=>{
-        res.send(err);
-    })
-})
-
-router.get("/emails", function(req, res, next) {
-    emails.getEmails().then(data=> {
-        res.render("admin/emails", admin.getParams(req, {
-            data
-        }));
-    })
-})
-
-router.delete("/emails/:id", function(req, res, next){ 
-    emails.delete(req.params.id).then(results=>{
-        io.emit('dashboard update')
-        res.send(results);
-    }).catch(err=>{
-        res.send(err);
-    })
-})
-
-router.get("/menus", function(req, res, next) {
-    menus.getMenus().then(data => {
-        res.render("admin/menus", admin.getParams(req, {
-            data
-        }))
-    })
-})
-
-router.post("/menus", function(req, res, next) {
-    menus.save(req.fields, req.files).then(results => {
-        io.emit('dashboard update')
-        res.send(results);
-    }).catch(err => {
-        console.error("ERRO AO SALVAR O MENU:", err); 
-        res.send(err);
+    router.get("/dashboard", async function(req, res, next){
+        try {
+            let data = await reservations.dashboard();
+            res.json(data); 
+        } catch (err) {
+            res.status(500).json(err);
+        }
     });
-});
 
-router.delete("/menus/:id", function(req, res, next){
-    menus.delete(req.params.id).then(results=>{
-        io.emit('dashboard update')
-        res.send(results);
-    }).catch(err=>{
-        res.send(err)
-    })
-});
-
-router.get("/reservations", function(req, res, next) {
-    let start = (req.query.start) ? req.query.start : moment().subtract(1, "year").format("YYYY-MM-DD");
-    let end = (req.query.end) ? req.query.end : moment().format("YYYY-MM-DD");
-
-    reservations.getReservations(req).then(pag => {
-        res.render("admin/reservations", admin.getParams(req, {
-            date: {
-                start, 
-                end    
-            },
-            data: pag.data,
-            moment,
-            links: pag.links
-        }));
+    router.get("/logout", function(req, res, next) {
+        delete req.session.user;
+        res.redirect("/admin/login");
     });
-});
 
-router.get("/reservations/chart", function(req, res, next){
-    req.query.start = (req.query.start) ? req.query.start : moment().subtract(1, "year").format("YYYY-MM-DD");
-    req.query.end = (req.query.end) ? req.query.end : moment().format("YYYY-MM-DD");
-
-    reservations.chart(req).then(chartData=>{
-        res.send(chartData);
-    })
-})
-
-router.post("/reservations", function(req, res, next) {
-    reservations.save(req.fields, req.files).then(results => {
-        io.emit('dashboard update')
-        res.send(results);
-    }).catch(err => {
-        console.error("ERRO AO SALVAR O MENU:", err); 
-        res.send(err);
+    router.post("/login", async function(req, res, nxt) {
+        if(!req.body.email) {
+            users.render(req, res, "Preencha o campo e-mail");
+        } else if (!req.body.password) {
+            users.render(req, res, "Preencha o campo senha.");
+        } else {
+            try {
+                let user = await users.login(req.body.email, req.body.password);
+                req.session.user = user;
+                res.redirect("/admin");
+            } catch (err) {
+                users.render(req, res, err.message || err);
+            }
+        }
     });
-});
 
-router.delete("/reservations/:id", function(req, res, next){
-    reservations.delete(req.params.id).then(results=>{
-        res.send(results);
-        io.emit('dashboard update')
-    }).catch(err=>{
-        res.send(err)
-    })
-});
+    router.get("/login", function(req, res, next) {
+        users.render(req, res, null);
+    });
 
-router.get("/users", function(req, res, next) {
-    users.getUsers().then(data=>{
-        res.render("admin/users", admin.getParams(req, {
-            data
-        }))
-    })
-})
+    router.get("/contacts", async function(req, res, next) {
+        try {
+            let data = await contacts.getContacts();
+            res.render("admin/contacts", admin.getParams(req, {
+                data
+            }));
+        } catch (err) {
+            next(err);
+        }
+    });
 
-router.post("/users", function(req, res, next) {
-    users.save(req.fields).then(results=> {
-        io.emit('dashboard update')
-        res.send(results);
-    }).catch(err=> {
-        res.render("admin/users", admin.getParams(req))
-    })
-});
+    router.delete("/contacts/:id", async function(req, res, next){
+        try {
+            let results = await contacts.delete(req.params.id);
+            io.emit('dashboard update');
+            res.send(results);
+        } catch (err) {
+            res.send(err);
+        }
+    });
 
-router.post("/users/password-change", function(req, res, next){
-    users.changePassword(req).then(results=> {
-        io.emit('dashboard update')
-        res.send(results);
-    }).catch(err=> {
-        res.send({
-            error: err
-        })
-    })
-});
+    router.get("/emails", async function(req, res, next) {
+        try {
+            let data = await emails.getEmails();
+            res.render("admin/emails", admin.getParams(req, {
+                data
+            }));
+        } catch (err) {
+            next(err);
+        }
+    });
 
-router.delete("/users/:id", function(req, res, next) {
-    users.delete(req.params.id).then(results=> {
-        res.send(results);
-        io.emit('dashboard update')
-    }).catch(err=> {
-        res.send(err);
-    })
-})
+    router.delete("/emails/:id", async function(req, res, next){ 
+        try {
+            let results = await emails.delete(req.params.id);
+            io.emit('dashboard update');
+            res.send(results);
+        } catch (err) {
+            res.send(err);
+        }
+    });
+
+    router.get("/menus", async function(req, res, next) {
+        try {
+            let data = await menus.getMenus();
+            res.render("admin/menus", admin.getParams(req, {
+                data
+            }));
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    router.post("/menus", async function(req, res, next) {
+        try {
+            let results = await menus.save(req.fields, req.files);
+            io.emit('dashboard update');
+            res.send(results);
+        } catch (err) {
+            console.error("ERRO AO SALVAR O MENU:", err); 
+            res.send(err);
+        }
+    });
+
+    router.delete("/menus/:id", async function(req, res, next){
+        try {
+            let results = await menus.delete(req.params.id);
+            io.emit('dashboard update');
+            res.send(results);
+        } catch (err) {
+            res.send(err);
+        }
+    });
+
+    router.get("/reservations", async function(req, res, next) {
+        let start = (req.query.start) ? req.query.start : moment().subtract(1, "year").format("YYYY-MM-DD");
+        let end = (req.query.end) ? req.query.end : moment().format("YYYY-MM-DD");
+
+        try {
+            let pag = await reservations.getReservations(req);
+            res.render("admin/reservations", admin.getParams(req, {
+                date: {
+                    start, 
+                    end    
+                },
+                data: pag.data,
+                moment,
+                links: pag.links
+            }));
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    router.get("/reservations/chart", async function(req, res, next){
+        req.query.start = (req.query.start) ? req.query.start : moment().subtract(1, "year").format("YYYY-MM-DD");
+        req.query.end = (req.query.end) ? req.query.end : moment().format("YYYY-MM-DD");
+
+        try {
+            let chartData = await reservations.chart(req);
+            res.send(chartData);
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    router.post("/reservations", async function(req, res, next) {
+        try {
+            let results = await reservations.save(req.fields, req.files);
+            io.emit('dashboard update');
+            res.send(results);
+        } catch (err) {
+            console.error("ERRO AO SALVAR A RESERVA:", err); 
+            res.send(err);
+        }
+    });
+
+    router.delete("/reservations/:id", async function(req, res, next){
+        try {
+            let results = await reservations.delete(req.params.id);
+            io.emit('dashboard update');
+            res.send(results);
+        } catch (err) {
+            res.send(err);
+        }
+    });
+
+    router.get("/users", async function(req, res, next) {
+        try {
+            let data = await users.getUsers();
+            res.render("admin/users", admin.getParams(req, {
+                data
+            }));
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    router.post("/users", async function(req, res, next) {
+        try {
+            let results = await users.save(req.fields);
+            io.emit('dashboard update');
+            res.send(results);
+        } catch (err) {
+            res.render("admin/users", admin.getParams(req));
+        }
+    });
+
+    router.post("/users/password-change", async function(req, res, next){
+        try {
+            let results = await users.changePassword(req);
+            io.emit('dashboard update');
+            res.send(results);
+        } catch (err) {
+            res.send({
+                error: err
+            });
+        }
+    });
+
+    router.delete("/users/:id", async function(req, res, next) {
+        try {
+            let results = await users.delete(req.params.id);
+            io.emit('dashboard update');
+            res.send(results);
+        } catch (err) {
+            res.send(err);
+        }
+    });
 
     return router;
 };
--- routes/index.js
+++ routes/index.js
@@ -8,98 +8,106 @@ var router = express.Router();
 
 module.exports = function(io) {
   /* GET home page. */
-router.get('/', function(req, res, next) {
-  menus.getMenus().then(results => {
+  router.get('/', async function(req, res, next) {
+    try {
+      let results = await menus.getMenus();
       res.render('index', { 
         title: 'Restaurante Saboroso!',
         menus: results,
         isHome: true,
         background: 'images/img_bg_1.jpg'
       });
+    } catch (err) {
+      next(err);
+    }
   });
-});
 
-router.get('/contacts', function(req, res, next) {
-  contacts.render(req, res);
-});
+  router.get('/contacts', function(req, res, next) {
+    contacts.render(req, res);
+  });
 
-router.post('/contacts', function(req, res, next) {
+  router.post('/contacts', async function(req, res, next) {
+    if (!req.body.name) {
+      contacts.render(req, res, 'Digite o nome');
+    } else if(!req.body.email) {
+      contacts.render(req, res, 'Digite o email');
+    } else if(!req.body.message) {
+      contacts.render(req, res, 'Digite a mensagem');
+    } else {
+      try {
+        let results = await contacts.save(req.body);
+        req.body = {};
+        io.emit('dashboard update');
+        contacts.render(req, res, null, "Contato enviado com sucesso!");
+      } catch (err) {
+        contacts.render(req, res, err.message);
+      }
+    }
+  });
 
-  if (!req.body.name) {
-    contacts.render(req, res, 'Digite o nome');
-  } else if(!req.body.email) {
-    contacts.render(req, res, 'Digite o email');
-  } else if(!req.body.message) {
-    contacts.render(req, res, 'Digite a mensagem');
-  } else {
-    contacts.save(req.body).then(results=> {
-      req.body = {};
-      io.emit('dashboard update');
-      contacts.render(req, res, null, "Contato enviado com sucesso!")
-    }).catch(err=> {
-      contacts.render(req, res, err.message)
-    })
-  }
-});
+  router.get('/menus', async function(req, res, next) {
+    try {
+      let results = await menus.getMenus();
+      res.render('menus', { 
+        title: 'Menus - Restaurante Saboroso!',
+        background: 'images/img_bg_1.jpg',
+        h1: 'Saboreie nosso menu!',
+        isHome: false 
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
 
-router.get('/menus', function(req, res, next) {
-  menus.getMenus().then(results => {
-    res.render('menus', { 
-      title: 'Menus - Restaurante Saboroso!',
+  router.get('/reservations', function(req, res, next) {
+      res.render('reservations', {
+          title: 'Reservas - Restaurante Saboroso',
+          background: 'images/img_bg_2.jpg',
+          h1: 'Reserve uma Mesa!',
+          body: {} 
+      });
+  });
+
+  router.post('/reservations', async function(req, res, next) {
+    if(!req.body.name) {
+      reservations.render(req, res, "Digite um nome");
+    } else if (!req.body.email) {
+      reservations.render(req, res, "Digite um email válido");
+    } else if (!req.body.people) {
+      reservations.render(req, res, "Escolha um número de pessoas");
+    } else if (!req.body.date) {
+      reservations.render(req, res, "Digite uma data");
+    } else if (!req.body.time) {
+      reservations.render(req, res, "Digite um horário");
+    } else {
+      try {
+        let results = await reservations.save(req.body);
+        req.body = {};
+        io.emit('dashboard update');
+        reservations.render(req, res, null, "Reserva realizada!");
+      } catch (err) {
+        reservations.render(req, res, err.message);
+      }
+    }
+  });
+
+  router.get('/services', function(req, res, next) {
+    res.render('services', { 
+      title: 'Serviços - Restaurante Saboroso!',
       background: 'images/img_bg_1.jpg',
-      h1: 'Saboreie nosso menu!',
+      h1: 'É um prazer poder servir!',
       isHome: false 
     });
-  })
-});
-
-router.get('/reservations', function(req, res, next) {
-    res.render('reservations', {
-        title: 'Reservas - Restaurante Saboroso',
-        background: 'images/img_bg_2.jpg',
-        h1: 'Reserve uma Mesa!',
-        body: {} 
-    });
-});
-
-router.post('/reservations', function(req, res, next) {
-  if(!req.body.name) {
-    reservations.render(req, res, "Digite um nome");
-  } else if (!req.body.email) {
-    reservations.render(req, res, "Digite um email válido");
-  } else if (!req.body.people) {
-    reservations.render(req, res, "Escolha um número de pessoas");
-  } else if (!req.body.date) {
-    reservations.render(req, res, "Digite uma data");
-  } else if (!req.body.time) {
-    reservations.render(req, res, "Digite um horário");
-  } else {
-    reservations.save(req.body).then(results=>{
-      req.body = {};
-      io.emit('dashboard update');
-      reservations.render(req, res, null, "Reserva realizada!")
-    }).catch(err=>{
-      reservations.render(req, res, err.message);
-    })
-  }
-});
-
-router.get('/services', function(req, res, next) {
-  res.render('services', { // <-- Corrigido para renderizar 'services'
-    title: 'Serviços - Restaurante Saboroso!',
-    background: 'images/img_bg_1.jpg',
-    h1: 'É um prazer poder servir!',
-    isHome: false 
   });
-});
 
-router.post("/subscribe", function(req, res, next) {
-    emails.save(req).then(results=>{
+  router.post("/subscribe", async function(req, res, next) {
+    try {
+      let results = await emails.save(req);
       res.send(results); 
-    }).catch(err=>{
+    } catch (err) {
       res.send(err);
-    })
-})
+    }
+  });
 
   return router;
-}
+};
