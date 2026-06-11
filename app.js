83198d3dcb47bb978eb5b0c2b5408b0051615c2c
Celso Bitello <celsokaiiki@gmail.com>
2026-06-11 01:27:41 -0300

    Adicionando .env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -62,7 +63,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(session({
-  secret:'p@ssw@ord',
+  secret: process.env.SESSION_SECRET,
   resave:true,
   saveUninitialized:true 
 }));
